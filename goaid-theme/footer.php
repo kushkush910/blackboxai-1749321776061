e9e2b049e3b59a59318bb8f1a8e205007a320b1d
kushkush910 <adirathdigital@gmail.com>
2025-06-08 00:13:04 +0530

    Add goaid-theme/footer.php 1749321783358

---
+++ goaid-theme/footer.php
@@ -0,0 +1,29 @@
+<footer class="bg-gray-100 text-gray-700 py-8 mt-12">
+    <div class="container mx-auto px-4">
+        <div class="flex flex-col md:flex-row justify-between">
+            <div class="mb-6 md:mb-0">
+                <h3 class="font-bold text-lg mb-2">Get in touch</h3>
+                <p>Jaipur: 91, Parvati Marg, Hanuman Nagar Extension, Paschim Vihar, Vaishali Nagar, Jaipur, Rajasthan 302021</p>
+                <p>Delhi: N 62, Humayunpur, Safdarjung Enclave, Delhi, New Delhi, Delhi 110029</p>
+                <p>Phone: <a href="tel:+918008280020" class="text-blue-600 hover:underline">+91-80082-80020</a></p>
+                <p>Phone: <a href="tel:+9109015908908" class="text-blue-600 hover:underline">+91-90159-08908</a></p>
+                <p>Email: <a href="mailto:support@goaid.in" class="text-blue-600 hover:underline">support@goaid.in</a></p>
+            </div>
+            <div class="mb-6 md:mb-0">
+                <h3 class="font-bold text-lg mb-2">Follow Us</h3>
+                <div class="flex space-x-4">
+                    <a href="https://www.facebook.com/goaidofficial/" class="hover:underline">Facebook</a>
+                    <a href="https://www.linkedin.com/company/goaid/" class="hover:underline">LinkedIn</a>
+                    <a href="https://www.instagram.com/goaidofficial/" class="hover:underline">Instagram</a>
+                    <a href="https://twitter.com/GoAidofficial" class="hover:underline">Twitter</a>
+                </div>
+            </div>
+            <div>
+                <p class="text-sm">&copy; <?php echo date('Y'); ?> GoAid. All Rights Reserved.</p>
+            </div>
+        </div>
+    </div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
